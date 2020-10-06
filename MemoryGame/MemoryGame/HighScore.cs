c19f0c58f189458ed5da1d81b8093a44510ee482
SDanDyS <gameking23@hotmail.com>
2020-10-06 01:41:30 -0700

    testing

--- MemoryGame/MemoryGame/HighScore.cs
+++ MemoryGame/MemoryGame/HighScore.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
+using System.Xml;
+using Security;
 
 namespace MemoryGame
 {
@@ -10,21 +12,21 @@ namespace MemoryGame
     /// </summary>
     struct HighScoreListing
     {
-        public string Name { get; private set; }
-        public int Score { get; private set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
 
-        public HighScoreListing(string playerName, int playerScore)
-        {
-            /*  This class is used to store a listing from the HighScore class. 
-             *  After the memory game ends I will instantiate this class thus calling this constructor. 
-             *  
-             *  When this class is instantiated it will recieve a (string)playerName and a (int)playerScore. 
-             *  You will have to communicate this with Daniel and Wietze because the highscores should be saved
-             *  to a file and loaded from a file. 
-             */
-            this.Name = playerName;
-            this.Score = playerScore;
-        }
+        //public HighScoreListing(string playerName, int playerScore)
+        //{
+        //    /*  This class is used to store a listing from the HighScore class. 
+        //     *  After the memory game ends I will instantiate this class thus calling this constructor. 
+        //     *  
+        //     *  When this class is instantiated it will recieve a (string)playerName and a (int)playerScore. 
+        //     *  You will have to communicate this with Daniel and Wietze because the highscores should be saved
+        //     *  to a file and loaded from a file. 
+        //     */
+        //    this.Name = playerName;
+        //    this.Score = playerScore;
+        //}
     }
 
     /// <summary>
@@ -32,8 +34,9 @@ namespace MemoryGame
     /// </summary>
     class HighScore
     {
-        public List<HighScoreListing> highScores { get; set; }
-        public Files HighScorePath { get; private set; } = new Files(Path.Combine(Directory.GetCurrentDirectory(), "highscores.txt"));
+        private int MaximumEntries;
+
+        public List<HighScoreListing> HighScores { get; set; }
         public HighScore()
         {
             /*  The highscores class is used to populate the table in the HighScoresTab in tabcontrol.
@@ -61,15 +64,82 @@ namespace MemoryGame
              *    sorts out the top (x) highest scoring games!
              *    
              */
-            this.highScores = new List<HighScoreListing>();
+            this.HighScores = new List<HighScoreListing>();
+            this.GetHighScores(15);
         }
 
         public void AddToHighScores(Player player)
         {
-            HighScoreListing listing = new HighScoreListing(player.Name, player.ScoreBoard.Score);
-            this.highScores.Add(listing);
-            string json = JsonConvert.SerializeObject(this.highScores);
-            this.HighScorePath.WriteToFile(json);
+            /*
+             * Add to the highscores list
+             */
+            HighScoreListing listing = new HighScoreListing { Name = player.Name, Score = player.ScoreBoard.Score };
+            this.HighScores.Add(listing);
+
+            /*
+             * Convert the list to JSON
+             */
+            string json = JsonConvert.SerializeObject(this.HighScores, Newtonsoft.Json.Formatting.Indented);
+
+            /*
+             * Encrypt the JSON file
+             */
+            Encryptor EncryptorObj = new Encryptor();
+            string encryptedJson = EncryptorObj.Encrypt(json);
+
+            /*
+             * Create the filepath and write the encrypted JSON format to file
+             */
+            Files.Create(Path.Combine(Directory.GetCurrentDirectory(), "highscores.txt"));
+            Files.WriteToFile(Path.Combine(Directory.GetCurrentDirectory(), "highscores.txt"), encryptedJson, true);
+        }
+
+        /*
+         * this.HighScores is populated with the file its contents (and can be accessed)
+         * You get the List returned in case any mutation is required or any specific handling of the List is required
+         * If not needed, don't catch it, if required, you can catch it and handle it in the way you want
+         */
+        public List<HighScoreListing> GetHighScores(int limit)
+        {
+            /*
+             * Get the content of the file (highscores)
+             */
+            string moppie = Files.GetFileContent(Path.Combine(Directory.GetCurrentDirectory(), "highscores.txt"));
+            /*
+             * If the content of the file isn't empty, decode it
+             */
+            if (moppie.Length > 0)
+            {
+                /*
+                 * Decrypt the JSON file
+                 */
+                Encryptor EncryptorObj = new Encryptor();
+                EncryptorObj.Decrypt(moppie);
+
+                /*
+                 * Decode the JSON and assign it to the LIST
+                 */
+                this.HighScores = JsonConvert.DeserializeObject<List<HighScoreListing>>(moppie);
+            }
+
+            return this.HighScores;
+        }
+
+        public List<HighScoreListing> Sort(string Hierarchy)
+        {
+            /*
+             * ascending is rom lowest value to highest value
+             * descending is from highest value to lowest value
+             */
+            if (Hierarchy == "ascending")
+            {
+                this.HighScores.Sort((x, y) => x.Score.CompareTo(y.Score));
+            }
+            else if (Hierarchy == "descending")
+            {
+                this.HighScores.Sort((x, y) => y.Score.CompareTo(x.Score));
+            }
+            return this.HighScores;
         }
     }
 }
